e2e7678a0db05c3f5ae057c93ec75f2a40cad8b7
jamesjeong94 <jamesjeong94@gmail.com>
2020-06-18 19:47:57 -0400

    completed WordBreak

--- 300-LongestIncSubSeq/300-LongestIncSubSeq.js
+++ 300-LongestIncSubSeq/300-LongestIncSubSeq.js
@@ -24,9 +24,7 @@ var lengthOfLIS = function (nums) {
       globalMax = dp[i];
     }
   }
-  console.log(dp);
   return globalMax;
 };
 
 console.log(lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]));
-console;
