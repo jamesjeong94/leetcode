575e82b64f3598910994c727117cd2093c2cd00e
jamesjeong94 <jamesjeong94@gmail.com>
2020-06-20 20:35:20 -0400

    implemented ValidAnagram

--- 1029-TwoCity/1029-TwoCity.js
+++ 1029-TwoCity/1029-TwoCity.js
@@ -1,1 +1,22 @@
-const twoCitySchedCost = function (costs) {};
+const twoCitySchedCost = function (costs) {
+  let totalCost = 0;
+  let n = costs.length / 2;
+  costs.sort((a, b) => {
+    return a[0] - b[0] - (a[1] - b[1]);
+  });
+  for (let i = 0; i < n; i++) {
+    totalCost += costs[i][0] + costs[i + n][1];
+  }
+  return totalCost;
+};
+
+console.log(
+  twoCitySchedCost([
+    [259, 770],
+    [448, 54],
+    [926, 667],
+    [184, 139],
+    [840, 118],
+    [577, 469],
+  ])
+);
