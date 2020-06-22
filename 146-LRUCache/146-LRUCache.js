026c2410e3d8550cbacd182366f7754ca3ed65cc
James Jeong <jamesjeong94@gmail.com>
2020-06-21 21:33:34 -0400

    dasd

--- 146-LRUCache/146-LRUCache.js
+++ 146-LRUCache/146-LRUCache.js
@@ -1,16 +1,9 @@
-/**
- * @param {number} capacity
- */
 const LRUCache = function (capacity) {
   this.cap = capacity;
   this.cache = new Map();
   this.count = 0;
 };
 
-/**
- * @param {number} key
- * @return {number}
- */
 LRUCache.prototype.get = function (key) {
   if (!this.cache.get(key)) {
     return -1;
@@ -22,33 +15,15 @@ LRUCache.prototype.get = function (key) {
   }
 };
 
-/**
- * @param {number} key
- * @param {number} value
- * @return {void}
- */
 LRUCache.prototype.put = function (key, value) {
   if (!this.cache.has(key)) {
     this.count++;
     if (this.count > this.cap) {
       let tempCount;
-      console.log('delete last cache');
       this.cache.delete(this.cache.keys().next().value);
     }
   } else {
     this.cache.delete(key);
   }
   this.cache.set(key, value);
-  console.log(this.cache);
 };
-
-let obj = new LRUCache(2);
-obj.put(1, 1);
-obj.put(2, 2);
-obj.get(2);
-obj.put(3, 3);
-obj.get(2);
-obj.put(4, 4);
-obj.get(1);
-obj.get(3);
-obj.get(4);
